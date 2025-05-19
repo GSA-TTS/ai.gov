573adc4faca3869916881184a8cb194fa6c89777
Khayal Alasgarov <khayal.alasgarov@gsa.gov>
2025-05-19 15:37:10 -0700

    feat: update button styles to use CSS variables for consistency and maintainabilit TCKT-29

--- apps/site/src/components/NavActionButtons.svelte
+++ apps/site/src/components/NavActionButtons.svelte
@@ -14,19 +14,18 @@
 </div>
 
 <style>
+  .ai-menu-buttons {
+    display: flex;
+    gap: 1rem;
+  }
   @media (max-width: 64em) {
     .ai-menu-buttons {
       flex-direction: column;
       padding: 1rem;
-      margin-bottom: 1.5rem;;
+      margin-bottom: 1.5rem;
       position: absolute;
       bottom: 0;
       width: 90%;
     }
-  }
-
-  .ai-menu-buttons {
-    display: flex;
-    gap: 1rem;
   }
 </style>
