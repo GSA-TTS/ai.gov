20259fa64350cc2e4e0e862cce889f6d94565226
Khayal Alasgarov <khayal.alasgarov@gsa.gov>
2025-06-02 09:59:25 -0700

    feat: create hero cards with slots and extract header navigation into a separate Astro file TCKT-91

---
+++ packages/ui/components/HeroCard.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let background = '#D7DAE0';
+  export let textColor = '#000';
+</script>
+
+<section>
+  <div
+    class="grid-row ai-hero-card__row"
+    style={`background: ${background}; color: ${textColor};`}
+  >
+    <div class="desktop:grid-col-5 padding-y-5 padding-x-4">
+      <div>
+        <slot name="iconRow" />
+        <slot name="title" />
+        <slot name="description" />
+      </div>
+      <slot name="buttons" />
+    </div>
+    <slot name="heroImage" />
+  </div>
+</section>
