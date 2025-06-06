2c9ec72e0ab6c7a6a0af58c6599915666e92c7b1
Khayal Alasgarov <khayal.alasgarov@gsa.gov>
2025-06-06 07:52:25 -0700

    feat: add featureCardsData.ts with data for feature cards TCKT-79

---
+++ apps/site/src/data/featureCardsData.ts
@@ -0,0 +1,20 @@
+export const featureCardData = [
+  {
+    iconPath: '/assets/images/stacked-arrows.svg',
+    title: 'Reduce operational costs while improving services',
+    description:
+      'Utilize the most advanced AI assistants for research, problem-solving, and guidance - helping you work smarter and more efficiently.',
+  },
+  {
+    iconPath: '/assets/images/progress-arrows.svg',
+    title: 'Make smarter decisions, faster',
+    description:
+      'Use Console to survey AI usage, enhance data-driven decision-making capabilities, and improve collaboration between agencies and departments.',
+  },
+  {
+    iconPath: '/assets/images/stacked-modules.svg',
+    title: 'Strengthen cybersecurity and infrastructure',
+    description:
+      'AI-powered systems continuously monitor digital environments, spotting potential threats in real-time and adapting defenses faster than traditional methods.',
+  },
+];
