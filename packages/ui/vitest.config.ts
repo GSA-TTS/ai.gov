aa5daf1495eb4698a91c5b9b42a82e32540ad02c
ethangardner <ethan.gardner@gsa.gov>
2025-06-06 10:55:31 -0400

    add test config to restore test funciton

---
+++ packages/ui/vitest.config.ts
@@ -0,0 +1,24 @@
+/// <reference types='vitest' />
+import {getViteConfig} from 'astro/config';
+import {mergeConfig} from 'vitest/config';
+import {baseConfig} from '@repo/vitest-config/base';
+
+export default getViteConfig(
+  mergeConfig(baseConfig, {
+    test: {
+      coverage: {
+        exclude: [
+          'dist/**',
+          'static/**',
+          'types/**',
+          'turbo/**',
+          '**/*.config.*',
+          '**/*.svelte'
+        ]
+      },
+      include: [
+        'src/**/*.test.ts'
+      ]
+    },
+  })
+);
